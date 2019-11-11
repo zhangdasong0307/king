9b860dc8fef1db999f1f25228b9b288759bcd529
zhangdasong0307 <451471253@qq.com>
2019-11-11 16:06:35 +0800

    first commit

---
+++ test_case/test_prod.py
@@ -0,0 +1,196 @@
+from tools.api import request_tool
+from tools.security.md5_tool import md5_passwd
+
+'''
+自动生成 数字 20,80   #生成20到80之间的数字 例：56
+自动生成 字符串 5 中文数字字母特殊字符 xuepl        #生成以xuepl开头加上长度2到5位包含中文数字字母特殊字符的字符串，例子：xuepl我1
+自动生成 地址
+自动生成 姓名
+自动生成 手机号
+自动生成 邮箱
+自动生成 身份证号
+'''
+
+def test_post_json(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "产品模块"  # allure报告中一级分类
+    story = '增加产品'  # allure报告中二级分类
+    title = "全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/addProd"  # 接口地址
+    headers = {'token':pub_data['token']}
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    json_data = '''
+    {
+  "brand": "苹果",
+  "colors": [
+    "炫酷黑","玫瑰金","星空灰"
+  ],
+  "price": 8888,
+  "productCode": "自动生成 字符串 5 数字字母",
+  "productName": "苹果4",
+  "sizes": [
+    "5寸"
+  ],
+  "type": "手机"
+}
+    '''
+    status_code = 200  # 响应状态码
+    expect = ""  # 预期结果
+    json_path = [{'skuCode':'$.data[0].skuCode'}]    #等于pub_data['productCode'] = r.json()["data"][0]["productCode"]
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    r=request_tool.request(json_path=json_path,headers=headers,method=method,url=uri,pub_data=pub_data,json_data=json_data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+    pub_data['productCode'] = r.json()["data"][0]["productCode"]
+
+def test_change_Price(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "产品模块"  # allure报告中一级分类
+    story = '修改商品价格'  # allure报告中二级分类
+    title = "修改价格_全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/changePrice"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    data = {"SKU":'${skuCode}',"price":9999}
+    headers={"token":pub_data["token"]}
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    r = request_tool.request(headers=headers,method=method,url=uri,pub_data=pub_data,data=data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+
+
+def test_post_data_prodCode(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "产品模块"  # allure报告中一级分类
+    story = '批量修改价格'  # allure报告中二级分类
+    title = "批量修改价格_全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/changePriceByProdCode"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    data = {"prodCode":pub_data["productCode"],"price":12999}
+    headers={"token":pub_data["token"]}
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,data=data,status_code=status_code,expect=expect,feature=feature,story=story,title=title,headers=headers)
+
+def test_data_sold_Out(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "产品模块"  # allure报告中一级分类
+    story = '产品下架'  # allure报告中二级分类
+    title = "产品下架_全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/soldOut"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    data = {"productCode":pub_data["productCode"]}
+    headers={"token":pub_data["token"]}
+    status_code = 200  # 响应状态码
+    expect = ""  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,data=data,status_code=status_code,expect=expect,feature=feature,story=story,title=title,headers=headers)
+
+def test_toPreSale(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "产品模块"  # allure报告中一级分类
+    story = '产品预售'  # allure报告中二级分类
+    title = "产品预售_全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/toPreSale"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    data = {"productCode":pub_data["productCode"]}
+    headers={"token":pub_data["token"]}
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,data=data,status_code=status_code,expect=expect,feature=feature,story=story,title=title,headers=headers)
+
+def test_post_data_incrementSku(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "库存模块"  # allure报告中一级分类
+    story = '增量单个商品'  # allure报告中二级分类
+    title = "增量单个商品_全字段正常流_1"  # allure报告中用例名字
+    uri = "/product/incrementSku"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    data = {"skuCode":'${skuCode}','qty':1000}
+    headers={"token":pub_data["token"]}
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,data=data,status_code=status_code,expect=expect,feature=feature,story=story,title=title,headers=headers)
+
+
+def test_post_json_addOrder(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "订单模块"  # allure报告中一级分类
+    story = '无加密下单'  # allure报告中二级分类
+    title = "无加密下单_全字段正常流_1"  # allure报告中用例名字
+    uri = "/order/addOrder"  # 接口地址
+    headers = {"token":pub_data["token"]}
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    json_data = '''
+    {
+  "ordeerPrice":12999,
+  "orderLineList": [
+    {
+      "qty": 1,
+      "skuCode":"${skuCode}"
+    }
+  ],
+  "receiver": "xuepl",
+  "receiverPhone": "18103909786",
+  "receivingAddress": "上海,闵行区",
+  "sign": "",
+  "userName": "xuepl1111"
+}
+    '''
+    status_code = 200  # 响应状态码
+    expect = ""  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(headers=headers,method=method,url=uri,pub_data=pub_data,json_data=json_data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+
+
+def test_post_json_SignBody(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "订单模块"  # allure报告中一级分类
+    story = '数字签名下单'  # allure报告中二级分类
+    title = "数字签名下单_全字段正常流_1"  # allure报告中用例名字
+    uri = "/order/addOrderSignBody"  # 接口地址
+    headers = {"token":pub_data["token"]}
+    s='receiver=xuepl&ordeerPrice=12999&receiverPhone=18103909786&key=guoya'
+    sign=md5_passwd(s,"")
+    pub_data['sign']=sign
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    json_data = '''
+    {
+  "ordeerPrice":12999,
+  "orderLineList": [
+    {
+      "qty": 1,
+      "skuCode":"${skuCode}"
+    }
+  ],
+  "receiver": "xuepl",
+  "receiverPhone": "18103909786",
+  "receivingAddress": "上海,闵行区",
+  "sign": "${sign}",
+  "userName": "xuepl1111"
+}
+    '''
+    status_code = 200  # 响应状态码
+    expect = ""  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(headers=headers,method=method,url=uri,pub_data=pub_data,json_data=json_data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_case/test_user.py
@@ -0,0 +1,45 @@
+from tools.api import request_tool
+def test_post_json(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "用户模块"  # allure报告中一级分类
+    story = '用户注册'  # allure报告中二级分类
+    title = "全字段正常流_1"  # allure报告中用例名字
+    uri = "/signup"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    json_data = '''
+   {
+  "phone": "15901815752",
+  "pwd": "dasong0307",
+  "rePwd": "dasong0307",
+  "userName": "dasong0307"
+}
+    '''
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,json_data=json_data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+
+def test_post_json(pub_data):
+    method = "POST"  #请求方法，全部大写
+    feature = "用户模块"  # allure报告中一级分类
+    story = '用户登录'  # allure报告中二级分类
+    title = "全字段正常流_1"  # allure报告中用例名字
+    uri = "/login"  # 接口地址
+    # post请求json数据，注意数据格式为字典或者为json串 为空写None
+    json_data = '''
+   {
+  "pwd": "dasong0307",
+  "userName": "dasong0307"
+}
+    '''
+    status_code = 200  # 响应状态码
+    expect = "2000"  # 预期结果
+    # --------------------分界线，下边的不要修改-----------------------------------------
+    # method,pub_data和url为必传字段
+    request_tool.request(method=method,url=uri,pub_data=pub_data,json_data=json_data,status_code=status_code,expect=expect,feature=feature,story=story,title=title)
+
+
+
+
+
